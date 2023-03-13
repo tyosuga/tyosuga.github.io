f64701e693d6cf33ab75af1aee9cc33c87e685f9
tyosuga <tyosuga03@gmail.com>
2023-03-13 08:45:30 +0700

    perubahan2

--- js/script.js
+++ js/script.js
@@ -39,12 +39,12 @@ navbar.classList.remove('active');
 /*========== scroll reveal ==========*/
 ScrollReveal({
   reset: true,
-  distance: '10px',
+  distance: '30px',
   duration: 2000,
   delay: 200
 });
 
-ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
+ScrollReveal().reveal('.kolom, .diskripsi', { origin: 'top' });
 ScrollReveal().reveal('.home-img, .services-container, portfolio-box, .contact form', { origin: 'bottom' });
 ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
 ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
